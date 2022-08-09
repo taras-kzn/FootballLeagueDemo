368cf0184c107bd59ce2781776acceb7e352ea0e
denis <9172885049@mail.ru>
2022-08-09 18:56:04 +0300

    implemented mode canvas

--- FootballLeagueDemo/ViewController.swift
+++ FootballLeagueDemo/ViewController.swift
@@ -11,7 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .blue
+    }
+}
+
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView()
+            .edgesIgnoringSafeArea(.all)
+    }
+
+    struct ContainerView: UIViewControllerRepresentable {
+
+        let viewController = ViewController()
+
+        func makeUIViewController(context: Context) -> some ViewController {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        }
     }
 }
 
